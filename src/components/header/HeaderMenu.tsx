51b9cc26514270d8988a51782a8282083259fdaa
Mihir Jethwa <mihirjethwa7@gmail.com>
2021-02-08 17:02:25 +0530

    initial commit

--- src/components/header/HeaderMenu.tsx
+++ src/components/header/HeaderMenu.tsx
@@ -5,7 +5,7 @@ import './Header.css'
 import ATools from "../../assets/header/ATools..svg"
 import { Button } from 'antd'
 
-const HeaderMenu=()=> {
+const HeaderMenu=()=>{
     return (
         <Header className="header">
             <div className="logo">
